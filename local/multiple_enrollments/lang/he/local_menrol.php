d3e1406a3b1ee4f51d585d00382568765d83672c
Nadav Kavalerchik <nadavkav@gmail.com>
2015-06-24 19:44:33 +0300

    local/multiple_enrollments (init-M27)

---
+++ local/multiple_enrollments/lang/he/local_menrol.php
@@ -0,0 +1,26 @@
+<?php
+$string['pluginname'] = 'רישום משתמשים מרובים';
+
+$string['livetek_addons'] = 'ניהול משתמשים';
+$string['multipleenrollment'] = 'רישום משתמשים מרובים';
+$string['menrol_title'] = 'רישום משתמשים מרובים';
+$string['menrol_title_help'] = 'Multiple Enrollments Help!!!';
+
+
+
+$string['chooseenrollment'] = 'בחירת פעולת שיוך';
+$string['newenrollment'] = 'שיוך לתפקיד חדש';
+$string['existingenrollment'] = 'שיוך לתפקיד קיים';
+$string['existingcourse'] = 'קורסים בשימוש';
+$string['potentialcourse'] = 'קורסים זמינים';
+$string['selectuser'] = 'בחירת משתמש';
+$string['coursesrole'] = 'קורסים';
+$string['roleuser'] = 'משתמשים';
+$string['assignmessage'] = 'Courses Assigned Successfully';
+
+$string['enroloptions'] = 'Enrollment Options';
+$string['recovergrades'] = 'Recover user\'s old grades if possible';
+$string['enrolduration'] = 'Enrolment duration:';
+
+$string['menrol_course'] = 'שיוך';
+$string['munenrol_course'] = 'הסרה';
---
+++ local/multiple_enrollments/lang/he/local_multiple_enrollments.php
@@ -0,0 +1,26 @@
+<?php
+$string['pluginname'] = 'רישום משתמשים מרובים';
+
+$string['livetek_addons'] = 'ניהול משתמשים';
+$string['multipleenrollment'] = 'רישום משתמשים מרובים';
+$string['multiple_enrollments_title'] = 'רישום משתמשים מרובים';
+$string['multiple_enrollments_title_help'] = '<b>New Enrollment</b> - This is the default screen.
+												<br />The Users list has a list of users to whom no courses are assigned. You can select one or more users, select one or more courses and click on Enrol selected Users button to enrol the users to all the selected courses.
+												<br /><b>Existing Enrollment</b> - It lists users to whom one or more courses are assigned for the selected role. On selecting the user his existing and potential courses are listed. You can then assign more courses or unassign existing courses for these users.';
+$string['chooseenrollment'] = 'בחירת פעולת שיוך';
+$string['newenrollment'] = 'שיוך לתפקיד חדש';
+$string['existingenrollment'] = 'שיוך לתפקיד קיים';
+$string['existingcourse'] = 'קורסים בשימוש';
+$string['potentialcourse'] = 'קורסים זמינים';
+$string['selectuser'] = 'בחירת משתמש';
+$string['coursesrole'] = 'קורסים';
+$string['roleuser'] = 'משתמשים';
+$string['assignmessage'] = 'Courses Assigned Successfully';
+
+$string['enroloptions'] = 'Enrollment Options';
+$string['recovergrades'] = 'Recover user\'s old grades if possible';
+$string['enrolduration'] = 'Enrolment duration:';
+
+$string['multiple_enrollments_course'] = 'שיוך לתפקיד';
+$string['munenrol_course'] = 'הסרה מתפקיד';
+$string['select_multiple_options'] = 'Note: Press Ctrl key to select multiple options';
