2b942527c40c3bc80c6414e66699a13bbc5e7ca5
Nadav Kavalerchik <nadavkav@gmail.com>
2015-06-28 22:34:48 +0300

    blocks/sharing_cart (init - M27)

---
+++ blocks/sharing_cart/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2015012700;
+$plugin->requires  = 2012062501.00; // Moodle 2.3.1
+$plugin->component = 'block_sharing_cart';
+$plugin->release   = '2.6, release 1 patch 7';
+$plugin->maturity  = MATURITY_STABLE;
