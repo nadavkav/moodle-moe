c884f11ee37181c660c2bff4cc1e9c4e7587d796
Nadav Kavalerchik <nadavkav@gmail.com>
2015-06-28 17:22:51 +0300

    admin/tool/editrolesbycap (init-M28)

---
+++ admin/tool/editrolesbycap/lang/he/tool_editrolesbycap.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_editrolesbycap', language 'he', branch 'MOODLE_22_STABLE'
+ *
+ * @package    tool_editrolesbycap
+ * @copyright  2012 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['capabilityandname'] = '{$a->name} ({$a->capabilityname})';
+$string['capabilitynamewithrisks'] = '{$a->name} ({$a->capabilityname}) {$a->risks}';
+$string['checkandeditroles'] = 'בדיקה ועריכת הגדרות תפקידים';
+$string['editrolesfor'] = 'עריכת הגדרות תפקיד עבור ההרשאה {$a}';
+$string['nonematch'] = 'לא נמצאה הרשאה מתאימה, אנא נסו שוב';
+$string['pluginname'] = 'עריכת תפקיד לפי הרשאה';
+$string['roledescription'] = 'תאור תפקיד';
+$string['rolenameandshortname'] = '{$a->name} ({$a->shortname})';
+$string['selectacapability'] = 'בחירת הרשאה';
+$string['selectadifferentcapability'] = 'בחירת הרשאה שונה';
