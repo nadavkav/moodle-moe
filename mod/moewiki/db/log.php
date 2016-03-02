57feacadcd843b221a5183e7f6cd3d89db7b3d51
Avi Levy <avi@sysbind.co.il>
2016-03-02 23:24:39 +0200

    Ticket 7 - Fork ouwiki

---
+++ mod/moewiki/db/log.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Definition of log events
+ *
+ *
+ * @package    mod_moewiki
+ * @copyright  2012 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $DB;
+
+$logs = array(
+    array('module' => 'moewiki', 'action' => 'add', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'annotate', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'diff', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'edit', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'entirewiki', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'history', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'lock', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'participation', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'revert', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'search', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'unlock', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'update', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'userparticipation', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'versiondelete', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'versionundelete', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'view', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'view all', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'viewold', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'wikihistory', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'wikiindex', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'page created', 'mtable' => 'moewiki', 'field' => 'name'),
+    array('module' => 'moewiki', 'action' => 'page updated', 'mtable' => 'moewiki', 'field' => 'name')
+);
---
+++ mod/moewiki/override.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Handles what happens when a user with appropriate permission attempts to
+ * override a wiki page editing lock.
+ *
+ * @copyright &copy; 2007 The Open University
+ * @author s.marshall@open.ac.uk
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package moewiki
+ */
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require($CFG->dirroot.'/mod/moewiki/basicpage.php');
+
+if (!data_submitted()) {
+    print_error("Only POST requests accepted");
+}
+
+if (!has_capability('mod/moewiki:overridelock', $context)) {
+    print_error("You do not have the capability to override editing locks");
+}
+
+$pageversion = moewiki_get_current_page($subwiki, $pagename, MOEWIKI_GETPAGE_ACCEPTNOVERSION);
+moewiki_override_lock($pageversion->pageid);
+
+$redirpage = optional_param('redirpage', '', PARAM_ALPHA);
+
+if ($redirpage != '') {
+    redirect($redirpage.'.php?'.moewiki_display_wiki_parameters($pagename, $subwiki, $cm, MOEWIKI_PARAMS_URL), '', 0);
+} else {
+    redirect('edit.php?'.moewiki_display_wiki_parameters($pagename, $subwiki, $cm, MOEWIKI_PARAMS_URL), '', 0);
+}
