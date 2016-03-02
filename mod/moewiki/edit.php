aadf31a0b452b0f1f642d6336f658d7908e44243
Avi Levy <avi@sysbind.co.il>
2016-03-02 23:24:39 +0200

    Ticket 7 - Fork ouwiki

---
+++ mod/moewiki/edit.php
@@ -0,0 +1,494 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Edit page. Allows user to edit and/or preview wiki pages.
+ *
+ * @copyright &copy; 2007 The Open University
+ * @author s.marshall@open.ac.uk
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package moewiki
+ */
+
+require_once(dirname(__FILE__).'/../../config.php');
+require_once($CFG->dirroot.'/lib/ajax/ajaxlib.php');
+require_once($CFG->dirroot.'/mod/moewiki/basicpage.php');
+require_once($CFG->dirroot.'/mod/moewiki/edit_form.php');
+
+if (file_exists($CFG->dirroot.'/local/mobile/ou_lib.php')) {
+    require_once($CFG->dirroot.'/local/mobile/ou_lib.php');
+}
+
+$action = optional_param('editoption', '', PARAM_TEXT);
+
+// for creating pages and sections
+$frompage = optional_param('frompage', null, PARAM_TEXT);
+$newsection = optional_param('newsection', null, PARAM_TEXT);
+
+// for creating/editing sections
+$section = optional_param('section', null, PARAM_RAW);
+
+$urlparams = array();
+$urlparams['id'] = $cm->id;
+if (!empty($pagename)) {
+    $urlparams['page'] = $pagename;
+}
+if (!empty($newsection)) {
+    $urlparams['newsection'] = $newsection;
+}
+if (!empty($section)) {
+    $urlparams['section'] = $section;
+}
+
+// sort out if the action was save or cancel
+$save = $action === get_string('savechanges') ? true : false;
+$cancel = $action === get_string('cancel') ? true : false;
+
+if (!$cm = get_coursemodule_from_id('moewiki', $id)) {
+    print_error('invalidcoursemodule');
+}
+
+// Checking course instance
+$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+
+if (!$moewiki = $DB->get_record('moewiki', array('id' => $cm->instance))) {
+    print_error('invalidcoursemodule');
+}
+
+$PAGE->set_cm($cm);
+
+// When creating a new page, do some checks
+$addpage = false;
+if (!is_null($frompage)) {
+    $urlparams['frompage'] = $frompage;
+    $returnurl = new moodle_url('/mod/moewiki/view.php',
+            moewiki_display_wiki_parameters($frompage, $subwiki, $cm, MOEWIKI_PARAMS_ARRAY));
+    if (trim($pagename) === '') {
+        print_error('emptypagetitle', 'moewiki', $returnurl);
+    }
+    // Strip whitespace from new page name from form (editor does this for other links).
+    $pagename = preg_replace('/\s+/', ' ', $pagename);
+
+    $addpage = true;
+}
+
+$returnurl = new moodle_url('/mod/moewiki/view.php',
+        moewiki_display_wiki_parameters($pagename, $subwiki, $cm, MOEWIKI_PARAMS_ARRAY));
+
+// When creating a section, do checks
+$addsection = false;
+if (!is_null($newsection)) {
+    if (trim($newsection) === '') {
+        print_error('emptysectiontitle', 'moewiki', $returnurl);
+    }
+    $addsection = true;
+}
+
+$context = context_module::instance($cm->id);
+$PAGE->set_pagelayout('incourse');
+require_course_login($course, true, $cm);
+
+$moewikioutput = $PAGE->get_renderer('mod_moewiki');
+
+require_capability('mod/moewiki:edit', $context);
+
+$url = new moodle_url('/mod/moewiki/edit.php', $urlparams);
+$PAGE->set_url($url);
+
+// Check permission
+if (!$subwiki->canedit) {
+    print_error('You do not have permission to edit this wiki');
+}
+
+$useattachments = !$addsection && !$section;
+
+// create the new mform
+// customdata indicates whether attachments are used (no for sections)
+$mform = new mod_moewiki_edit_page_form('edit.php', (object)array(
+        'attachments' => $useattachments, 'startpage' => $pagename === '',
+        'addpage' => $addpage, 'addsection' => $addsection));
+
+// get form content if save/preview
+$content = null;
+$formdata = null;
+if ($formdata = $mform->get_data()) {
+    if ($content = $formdata->content['text']) {
+        // Check if they used the plaintext editor, if so fixup linefeeds
+        if ((isset($formdata->content['format'])) && ($formdata->content['format'] != FORMAT_HTML)) {
+            $content = moewiki_plain_to_xhtml($content);
+        }
+        $content = moewiki_format_xhtml_a_bit($content); // Tidy up HTML
+    }
+}
+
+// new content for section
+if ($newsection) {
+    $new = new StdClass;
+    $new->name = moewiki_display_user($USER, $course->id);
+    $new->date = userdate(time());
+    $sectionheader = html_writer::tag('h3', s($newsection)) .
+            html_writer::tag('p', '(' . get_string('createdbyon', 'moewiki', $new) . ')');
+}
+
+// if cancel redirect before going too far
+if ($cancel) {
+    // Get pageid to unlock.
+    $pageversion = moewiki_get_current_page($subwiki, $pagename);
+    if (!empty($pageversion->pageid)) {
+        moewiki_release_lock($pageversion->pageid);
+    }
+    redirect($returnurl);
+    exit;
+}
+
+// Get the current page version, creating page if needed
+$pageversion = moewiki_get_current_page($subwiki, $pagename, MOEWIKI_GETPAGE_CREATE);
+if ($addpage && !is_null($pageversion->xhtml)) {
+    print_error('duplicatepagetitle', 'moewiki', $returnurl);
+}
+if ($pageversion->locked === '1') {
+    print_error('thispageislocked', 'moewiki', 'view.php?id='.$cm->id);
+}
+
+// Need list of known sections on current version
+$knownsections = moewiki_find_sections($pageversion->xhtml);
+
+// Get section, make sure the name is valid
+if (!preg_match('/^[0-9]+_[0-9]+$/', $section)) {
+    $section = null;
+}
+if ($section) {
+    if (!array_key_exists($section, $knownsections)) {
+        print_error("Unknown section $section");
+    }
+    $sectiontitle = $knownsections[$section];
+    $sectiondetails = moewiki_get_section_details($pageversion->xhtml, $section);
+}
+
+
+// Get lock
+if (!$cancel) {
+    list($lockok, $lock) = moewiki_obtain_lock($moewiki, $pageversion->pageid);
+}
+
+if ($save) {
+    if (!$newsection ) {
+        // Check we started editing the right version
+        $startversionid = required_param('startversionid', PARAM_INT);
+        $versionok = $startversionid == $pageversion->versionid;
+    } else {
+        $versionok = true;
+    }
+
+    // If we either don't have a lock or are editing the wrong version...
+    if (!$versionok || !$lockok) {
+        $savefailtitle = get_string('savefailtitle', 'moewiki');
+        $specificmessage = get_string(!$versionok ? 'savefaildesynch' : 'savefaillocked', 'moewiki');
+        $returntoview = get_string('returntoview', 'moewiki');
+        $savefailcontent = get_string('savefailcontent', 'moewiki');
+        $actualcontent = moewiki_convert_content($content, $subwiki, $cm, null, $pageversion->xhtmlformat);
+
+        // we are either returning to an existing page or a "new" one that ws
+        // simultaneously created by someone else at the same time
+        $returnpage = $addpage ? $frompage : $pagename;
+
+        moewiki_release_lock($pageversion->pageid);
+        echo $OUTPUT->header();
+
+        $pagefield = '';
+        if ($returnpage !== '') {
+            $pagefield = html_writer::empty_tag('input', array('type' => 'hidden',
+                    'name' => 'page', 'value' => $returnpage));
+        }
+        print '<div id="moewiki_savefail">'
+            .'<h2>'.$savefailtitle.'</h2>'
+            .'<p>'.$specificmessage.'</p>'
+            .'<form action="view.php" method="get">'
+            .'<input type="hidden" name="id" value="'.$cm->id.'" />'
+            . $pagefield
+            .'<input type="submit" value="'.$returntoview.'" />'
+            .'</form>'
+            .'<p>'.$savefailcontent.'</p>'
+            .'<div class="moewiki_savefailcontent">'.$actualcontent.'</div>'
+            .'</div><div>';
+
+        moewiki_print_footer($course, $cm, $subwiki, $pagename);
+        exit;
+    }
+
+    $event = null;
+    if ($section) {
+        moewiki_save_new_version_section($course, $cm, $moewiki, $subwiki, $pagename, $pageversion->xhtml, $formdata->content['text'], $sectiondetails, $formdata);
+    } else {
+        if ($addpage) {
+            moewiki_create_new_page($course, $cm, $moewiki, $subwiki, $frompage, $pagename, $content, $formdata);
+        } else {
+            if ($addsection) {
+                moewiki_create_new_section($course, $cm, $moewiki, $subwiki, $pagename, $formdata->content['text'], $sectionheader, $formdata);
+            } else {
+                // Normal save
+                moewiki_save_new_version($course, $cm, $moewiki, $subwiki, $pagename, $content, -1, -1, -1, null, $formdata);
+            }
+        }
+    }
+
+    // Update completion state
+    $completion = new completion_info($course);
+    if ($completion->is_enabled($cm) && ($moewiki->completionedits || $moewiki->completionpages)) {
+        $completion->update_state($cm, COMPLETION_COMPLETE);
+    }
+
+    // Release lock, log and redirect.
+    moewiki_release_lock($pageversion->pageid);
+
+    // Log.
+    $info = '';
+    if ($pagename) {
+        $info = $pagename;
+    }
+
+    // Log usage edit.
+    $params = array(
+            'context' => $context,
+            'objectid' => $pageversion->pageid,
+            'other' => array('info' => $info, 'logurl' => $url->out_as_local_url())
+    );
+
+    if ($addpage) {
+        $event = \mod_moewiki\event\page_created::create($params);
+    } else {
+        $event = \mod_moewiki\event\page_updated::create($params);
+    }
+    $event->add_record_snapshot('course_modules', $cm);
+    $event->add_record_snapshot('course', $course);
+    $event->add_record_snapshot('moewiki', $moewiki);
+    $event->trigger();
+
+    // Redirect.
+    redirect($returnurl);
+    exit;
+}
+
+if ($section) {
+    $sectionfields = "<input type='hidden' name='section' value='$section' />";
+} else {
+    $sectionfields = '';
+}
+
+// Handle case where page is locked by someone else
+if (!$lockok) {
+    echo $OUTPUT->header();
+
+    $lockholder = $DB->get_record('user', array('id' => $lock->userid));
+    $canoverride = has_capability('mod/moewiki:overridelock', $context);
+    $pagelockedtimeout = null;
+
+    $cancel = get_string('cancel');
+    $tryagain = get_string('tryagain', 'moewiki');
+    $pagelockedtitle = get_string('pagelockedtitle', 'moewiki');
+    $overridelock = get_string('overridelock', 'moewiki');
+
+    $details = new StdClass;
+    $details->name = fullname($lockholder);
+    $details->lockedat = moewiki_nice_date($lock->lockedat);
+    $details->seenat = moewiki_nice_date($lock->seenat);
+    if ($lock->seenat > time()) {
+        // When the 'seen at' value is greater than current time, that means
+        // their lock has been automatically confirmed in advance because they
+        // don't have JavaScript support.
+        $details->nojs = moewiki_nice_date($lock->seenat + MOEWIKI_LOCK_PERSISTENCE);
+        $pagelockeddetails = get_string('pagelockeddetailsnojs', 'moewiki', $details);
+    } else {
+        $pagelockeddetails = get_string('pagelockeddetails', 'moewiki', $details);
+        if ($lock->expiresat) {
+            $pagelockedtimeout = get_string('pagelockedtimeout', 'moewiki', userdate($lock->expiresat));
+        }
+    }
+
+    $pagelockedoverride = $canoverride ? '<p>'.get_string('pagelockedoverride', 'moewiki').'</p>' : '';
+
+    print "<div id='moewiki_lockinfo'>
+        <h2>$pagelockedtitle</h2>
+        <p>$pagelockeddetails $pagelockedtimeout</p>
+        $pagelockedoverride
+        <div class='moewiki_lockinfobuttons'>";
+
+    if ($pagename === '') {
+        $pageinputs = '';
+    } else {
+        $pageinputs = html_writer::empty_tag('input', array('type' => 'hidden',
+                        'name' => 'page', 'value' => $pagename));
+    }
+    if ($addpage) {
+        $pageinputs .= html_writer::empty_tag('input', array('type' => 'hidden',
+                        'name' => 'frompage', 'value' => $frompage));
+    }
+
+    $newsectioninput = '';
+    if ($addsection) {
+        $newsectioninput = html_writer::empty_tag('input', array('type' => 'hidden',
+                'name' => 'newsection', 'value' => $newsection));
+    }
+
+    print "<form action='edit.php' method='get'>
+            <input type='hidden' name='id' value='$cm->id' />
+            $pageinputs
+            $newsectioninput
+            $sectionfields
+            <input type='submit' value='$tryagain' />
+        </form>";
+
+    print "<form action='view.php' method='get'>
+            <input type='hidden' name='id' value='$cm->id' />
+            $pageinputs
+            $newsectioninput
+            $sectionfields
+            <input type='submit' value='$cancel' />
+        </form>";
+
+    print $canoverride
+        ? "<form class='moewiki_overridelock' action='override.php' method='post'>
+        <input type='hidden' name='id' value='$cm->id' />
+        $pageinputs
+        $newsectioninput
+        <input type='submit' value='$overridelock' /></form>"
+        : '';
+
+    print "</div></div><div>";
+
+    moewiki_print_footer($course, $cm, $subwiki, $pagename);
+    exit;
+}
+
+// The page is now locked to us! Go ahead and print edit form
+
+// get title of the page
+$title = get_string('editingpage', 'moewiki');
+$wikiname = format_string(htmlspecialchars($moewiki->name));
+$name = '';
+if ($pagename) {
+    $title .= ': ' . $pagename;
+} else {
+    if ($addsection) {
+        $sectiontitle = $newsection;
+        $name = htmlspecialchars($newsection);
+        $title = get_string('editingsection', 'moewiki', $name);
+        $section = true;
+    } else {
+        if (!$section) {
+            $name = get_string('startpage', 'moewiki');
+        } else {
+            $name = htmlspecialchars($sectiontitle);
+            $title = get_string('editingsection', 'moewiki', $name);
+        }
+    }
+}
+$title = $wikiname.' - '.$title;
+
+echo $moewikioutput->moewiki_print_start($moewiki, $cm, $course, $subwiki, $pagename, $context,
+    array(array('name' =>
+        $section
+            ? get_string('editingsection', 'moewiki', htmlspecialchars($sectiontitle))
+            : get_string('editingpage', 'moewiki'), 'link' => null)
+        ), false, false, '', $title);
+
+if ($newsection) {
+    $section = false;
+}
+
+// Tabs
+moewiki_print_tabs('edit', $pagename, $subwiki, $cm, $context, $pageversion->versionid ? true : false);
+
+// setup the edit locking
+moewiki_print_editlock($lock, $moewiki);
+
+// Calculate initial text for editor
+if ($section) {
+    $existing = $sectiondetails->content;
+} else if ($newsection) {
+    $existing = $sectionheader;
+} else if ($pageversion) {
+    $existing = $pageversion->xhtml;
+} else {
+    $existing = '';
+}
+
+// print the preview box
+if ($content) {
+    echo $moewikioutput->moewiki_print_preview($content, $pagename, $subwiki, $cm, $pageversion->xhtmlformat);
+    $existing = $content;
+}
+
+// Get the annotations and add prepare them for editing
+$annotations = moewiki_get_annotations($pageversion);
+moewiki_highlight_existing_annotations($existing, $annotations, 'edit');
+
+print get_string('advice_edit', 'moewiki', $OUTPUT->help_icon('createlinkedwiki', 'moewiki'));
+if ($moewiki->timeout) {
+    $countdowntext = get_string('countdowntext', 'moewiki', $moewiki->timeout/60);
+    print "<script type='text/javascript'>
+                document.write('<p><div id=\"ouw_countdown\"></div>$countdowntext<span id=\"ouw_countdownurgent\"></span></p>');
+        </script>";
+}
+
+// Set up basic form data
+$data = new StdClass;
+$data->id = $cm->id;
+$data->startversionid = $pageversion->versionid;
+$data->page = $pagename;
+$data->frompage = $frompage;
+$data->newsection = $newsection;
+$data->section = $section;
+$data->user = $subwiki->userid;
+
+// Prepare form file manager attachments
+if ($useattachments) {
+    $attachmentsdraftid = file_get_submitted_draft_itemid('attachments');
+    file_prepare_draft_area($attachmentsdraftid, $context->id, 'mod_moewiki',
+            'attachment', empty($pageversion->versionid) ? null : $pageversion->versionid);
+    $data->attachments = $attachmentsdraftid;
+}
+
+// Prepare form editor attachments
+$contentdraftid = file_get_submitted_draft_itemid('content');
+$currenttext = file_prepare_draft_area($contentdraftid, $context->id, 'mod_moewiki', 'content',
+        empty($pageversion->versionid) ? null : $pageversion->versionid,
+        array('subdirs' => false), empty($existing) ? '' : $existing);
+
+$data->content = array('text' => $currenttext,
+       'format' => empty($pageversion->xhtmlformat)
+           ? editors_get_preferred_format() : $pageversion->xhtmlformat,
+       'itemid' => $contentdraftid);
+
+$mform->set_data($data);
+
+$mform->display();
+
+echo $moewikioutput->get_bottom_buttons($subwiki, $cm, $context, $pageversion, false);
+
+$stringlist = array(
+        array('savefailnetwork', 'moewiki'),
+        array('savefailtitle', 'moewiki'),
+);
+$jsmodule = array(
+        'name' => 'mod_moewiki_edit',
+        'fullpath' => '/mod/moewiki/module.js',
+        'requires' => array('base', 'event', 'io', 'node', 'anim', 'moodle-core-notification-alert', 'button'),
+        'strings'  => $stringlist
+);
+$PAGE->requires->js_init_call('M.mod_moewiki_edit.init', null, true, $jsmodule);
+
+// Footer
+moewiki_print_footer($course, $cm, $subwiki, $pagename);
---
+++ mod/moewiki/wikihistory.php
@@ -0,0 +1,276 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * 'Wiki changes' page. Displays a list of recent changes to the wiki. You
+ * can choose to view all changes or only new pages.
+ *
+ * @copyright &copy; 2007 The Open University
+ * @author s.marshall@open.ac.uk
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package moewiki
+ *//** */
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->dirroot.'/mod/moewiki/basicpage.php');
+require_once($CFG->dirroot.'/mod/moewiki/locallib.php');
+
+$id = required_param('id', PARAM_INT); // Course Module ID
+$newpages = optional_param('type', '', PARAM_ALPHA) == 'pages';
+$from = optional_param('from', '', PARAM_INT);
+
+$url = new moodle_url('/mod/moewiki/wikihistory.php', array('id' => $id));
+$PAGE->set_url($url);
+
+if ($id) {
+    if (!$cm = get_coursemodule_from_id('moewiki', $id)) {
+        print_error('invalidcoursemodule');
+    }
+
+    // Checking course instance
+    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+
+    if (!$moewiki = $DB->get_record('moewiki', array('id' => $cm->instance))) {
+        print_error('invalidcoursemodule');
+    }
+
+    $PAGE->set_cm($cm);
+}
+$context = context_module::instance($cm->id);
+$PAGE->set_pagelayout('incourse');
+require_course_login($course, true, $cm);
+
+$moewikioutput = $PAGE->get_renderer('mod_moewiki');
+
+// Get basic wiki parameters
+$wikiparams = moewiki_display_wiki_parameters('', $subwiki, $cm);
+$tabparams = $newpages ? $wikiparams.'&amp;type=pages' : $wikiparams;
+
+// Get changes
+if ($newpages) {
+    $changes = moewiki_get_subwiki_recentpages($subwiki->id, $from, MOEWIKI_PAGESIZE+1);
+} else {
+    $changes = moewiki_get_subwiki_recentchanges($subwiki->id, $from, MOEWIKI_PAGESIZE+1);
+}
+
+// Check to see whether any change has been overwritten by being imported.
+$overwritten = false;
+foreach ($changes as $change) {
+    if (!empty($change->importversionid)) {
+        $overwritten = true;
+        break;
+    }
+}
+
+// Do header
+$atomurl = $CFG->wwwroot.'/mod/moewiki/feed-wikihistory.php?'.$wikiparams.
+    ($newpages?'&amp;type=pages' : '').'&amp;magic='.$subwiki->magic;
+$rssurl = $CFG->wwwroot.'/mod/moewiki/feed-wikihistory.php?'.$wikiparams.
+    ($newpages?'&amp;type=pages' : '').'&amp;magic='.$subwiki->magic.'&amp;format=rss';
+$meta = '<link rel="alternate" type="application/atom+xml" title="Atom feed" '.
+    'href="'.$atomurl.'" />';
+
+// bug #3542
+$wikiname = format_string(htmlspecialchars($moewiki->name));
+$title = $wikiname.' - '.get_string('wikirecentchanges', 'moewiki');
+
+echo $moewikioutput->moewiki_print_start($moewiki, $cm, $course, $subwiki,
+    $from > 0
+        ? get_string('wikirecentchanges_from', 'moewiki', (int)($from/MOEWIKI_PAGESIZE) + 1)
+        : get_string('wikirecentchanges', 'moewiki'),
+    $context, null, false, false, $meta, $title);
+
+// Print tabs for selecting all changes/new pages
+$tabrow = array();
+$tabrow[] = new tabobject('changes', 'wikihistory.php?'.$wikiparams,
+    get_string('tab_index_changes', 'moewiki'));
+$tabrow[] = new tabobject('pages', 'wikihistory.php?'.$wikiparams.'&amp;type=pages',
+    get_string('tab_index_pages', 'moewiki'));
+$tabs = array();
+$tabs[] = $tabrow;
+print_tabs($tabs, $newpages ? 'pages' : 'changes');
+print '<div id="moewiki_belowtabs">';
+
+if ($newpages) {
+    $pagetabname = get_string('tab_index_pages', 'moewiki');
+} else {
+    $pagetabname = get_string('tab_index_changes', 'moewiki');
+}
+print get_accesshide($pagetabname, 'h1');
+
+// On first page, show information
+if (!$from) {
+    print get_string('advice_wikirecentchanges_'
+        .($newpages ? 'pages' : 'changes'
+        .(!empty($CFG->moewikienablecurrentpagehighlight) ? '' : '_nohighlight')), 'moewiki').'</p>';
+}
+
+$strdate = get_string('date');
+$strtime = get_string('time');
+$strpage = get_string('page', 'moewiki');
+$strperson = get_string('changedby', 'moewiki');
+$strview = get_string('view');
+
+$strimport = '';
+if ($overwritten) {
+    $strimport = get_string('importedfrom', 'moewiki');
+}
+
+print "
+<table class='generaltable'>
+<thead>
+<tr><th scope='col'>$strdate</th><th scope='col'>$strtime</th><th scope='col'>$strpage</th>".
+($newpages?'':'<th><span class="accesshide">'.$strview.'</span></th>');
+if ($moewiki->enablewordcount) {
+    print "<th scope='col'>".get_string('words', 'moewiki')."</th>";
+}
+if ($overwritten) {
+    print '<th scope="col">' . $strimport . '</th>';
+}
+print "
+  <th scope='col'>$strperson</th></tr></thead><tbody>
+";
+
+$strchanges = get_string('changes', 'moewiki');
+$strview = get_string('view');
+$lastdate = '';
+$count = 0;
+foreach ($changes as $change) {
+    $count++;
+    if ($count > MOEWIKI_PAGESIZE) {
+        break;
+    }
+
+    $pageparams = moewiki_display_wiki_parameters($change->title, $subwiki, $cm);
+
+    $date = userdate($change->timecreated, get_string('strftimedate'));
+    if ($date == $lastdate) {
+        $date = '';
+    } else {
+        $lastdate = $date;
+    }
+    $time = moewiki_recent_span($change->timecreated).userdate($change->timecreated, get_string('strftimetime')).'</span>';
+
+    $page = $change->title ? htmlspecialchars($change->title) : get_string('startpage', 'moewiki');
+    if (!empty($change->previousversionid)) {
+        $changelink = " <small>(<a href='diff.php?$pageparams&amp;v2={$change->versionid}&amp;v1={$change->previousversionid}'>$strchanges</a>)</small>";
+    } else {
+        $changelink = ' <small>('.get_string('newpage', 'moewiki').')</small>';
+    }
+
+    $current = '';
+    if ($change->versionid == $change->currentversionid || $newpages) {
+        $viewlink = "view.php?$pageparams";
+        if (!$newpages && !empty($CFG->moewikienablecurrentpagehighlight)) {
+            $current =' class="current"';
+        }
+    } else {
+        $viewlink = "viewold.php?$pageparams&amp;version={$change->versionid}";
+    }
+
+    $change->id = $change->userid;
+    if ($change->id) {
+        $userlink = moewiki_display_user($change, $course->id);
+    } else {
+        $userlink = '';
+    }
+
+    if ($newpages) {
+        $actions = '';
+        $page = "<a href='$viewlink'>$page</a>";
+    } else {
+        $actions = "<td class='actions'><a href='$viewlink'>$strview</a>$changelink</td>";
+    }
+
+    // see bug #3611
+    if (!empty($current) && !empty($CFG->moewikienablecurrentpagehighlight)) {
+        // current page so add accessibility stuff
+        $accessiblityhide = '<span class="accesshide">'.get_string('currentversionof', 'moewiki').'</span>';
+        $dummy = $page;
+        $page = $accessiblityhide.$dummy;
+    }
+
+    print "
+<tr$current>
+  <td class='ouw_leftcol'>$date</td><td>$time</td><td>$page</td>
+  $actions";
+    if ($moewiki->enablewordcount) {
+        if (isset($change->previouswordcount)) {
+            $wordcountchanges = moewiki_wordcount_difference($change->wordcount,
+                    $change->previouswordcount, true);
+        } else {
+            // first page
+            $wordcountchanges = moewiki_wordcount_difference($change->wordcount, 0, false);
+        }
+        print "<td>$wordcountchanges</td>";
+    }
+    if ($overwritten) {
+        if (!empty($change->importversionid)) {
+            $selectedmoewiki = moewiki_get_wiki_details($change->importversionid);
+            print '<td>';
+            if ($selectedmoewiki->courseshortname) {
+                print $selectedmoewiki->courseshortname. '<br/>';
+            }
+            print $selectedmoewiki->name;
+            if ($selectedmoewiki->group) {
+                print '<br/>';
+                print '[[' .$selectedmoewiki->group. ']]';
+            } else if ($selectedmoewiki->user) {
+                print '<br/>';
+                print '[[' .$selectedmoewiki->user. ']]';
+            }
+            print '</td>';
+        } else {
+            print '<td></td>';
+        }
+    }
+    print "
+  <td class='ouw_rightcol'>$userlink</td>
+</tr>";
+}
+
+print '</tbody></table>';
+
+if (empty($changes)) {
+    echo get_string('nowikipages', 'moewiki');
+}
+
+if ($count > MOEWIKI_PAGESIZE || $from > 0) {
+    print '<div class="ouw_paging"><div class="ouw_paging_prev">&nbsp;';
+    if ($from > 0) {
+        $jump = $from - MOEWIKI_PAGESIZE;
+        if ($jump < 0) {
+            $jump = 0;
+        }
+        print link_arrow_left(get_string('previous', 'moewiki'),
+            'wikihistory.php?'.$tabparams. ($jump > 0 ? '&amp;from='.$jump : ''));
+    }
+    print '</div><div class="ouw_paging_next">';
+    if ($count > MOEWIKI_PAGESIZE) {
+        $jump = $from + MOEWIKI_PAGESIZE;
+        print link_arrow_right(get_string('next', 'moewiki'),
+            'wikihistory.php?'.$tabparams. ($jump > 0 ? '&amp;from='.$jump : ''));
+    }
+    print '&nbsp;</div></div>';
+}
+
+echo $moewikioutput->moewiki_get_feeds($atomurl, $rssurl);
+
+$pageversion = moewiki_get_current_page($subwiki, $pagename);
+echo $moewikioutput->get_bottom_buttons($subwiki, $cm, $context, $pageversion, false);
+
+// Footer
+moewiki_print_footer($course, $cm, $subwiki);
