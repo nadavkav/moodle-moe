37f79cdb60b6d89c28fa51f1df4d0f0e75ebfe1d
Avi Levy <avi@sysbind.co.il>
2016-03-17 09:16:58 +0200

    Ticket-7 mod_moewiki: on select text create icon form annotation

--- mod/moewiki/amd/src/annotation.js
+++ mod/moewiki/amd/src/annotation.js
@@ -6,17 +6,36 @@
  * @module mod_moewiki/annotation
  */
 
-define([ 'jquery' ], function($) {
+define([ 'jquery', 'core/yui' ], function($, Y) {
 	var annotation = {
-		merkannotaion : function() {
+	    confirmDialogue : null,
+	    displayDialogue: function(e) {
+            if (e) {
+                e.halt();
+            }
+            annotation.confirmDialogue.show();
+        },
+		merkannotaion : function(strings) {
 			var selection;
+			annotation.confirmDialogue = new M.core.dialogue({
+                headerContent: 'Annotation Form',
+                bodyContent: Y.one('#annotationFrom'),
+                draggable: true,
+                visible: false,
+                center: true,
+                modal: true,
+                width: null,
+                extraClasses: ['mod_mowiki_annotate_popup']
+            });
+			Y.one('#testmodal').on('click', annotation.displayDialogue);
 			$('.moewiki_content').mousedown(function() {
 				$('.moewiki_content').mouseup(function() {
 					try {
 			            if (selection = window.getSelection()) {
 			            	if(selection.type != 'Range'){
-			            		return ;
+			            		annotation.hide_annotaion(Y.one('#testmodal'));
 			            	}
+			            	annotation.show_annotaion(Y.one('#testmodal'));
 			            }
 			        } catch (e) {
 			            /* give up */
@@ -25,6 +44,18 @@ define([ 'jquery' ], function($) {
 			});
 
 		},
+		hide_annotaion: function(element){
+			element.setStyles({display: 'none'});
+		},
+		show_annotaion: function(element){
+			element.setStyles({display: 'block'});
+		},
+		displayDialogue: function(e) {
+            if (e) {
+                e.halt();
+            }
+            annotation.confirmDialogue.show();
+        },
 	};
 	return annotation;
 });
