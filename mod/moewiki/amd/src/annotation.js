91e87158976a53f4c725a508af41f47a02e651b7
Avi Levy <avi@sysbind.co.il>
2016-04-18 16:41:09 +0300

    ticket7 use open annotaion

--- mod/moewiki/amd/src/annotation.js
+++ mod/moewiki/amd/src/annotation.js
@@ -6,67 +6,48 @@
  * @module mod_moewiki/annotation
  */
 
-define([ 'jquery', 'core/yui', 'core/ajax'], function($, Y, ajax) {
+define([ 'jquery', 'mod_moewiki/annotator', 'core/ajax'], function($, annotator, ajax) {
 	var annotation = {
-	    confirmDialogue : null,
-	    displayDialogue: function(e) {
-            if (e) {
-                e.halt();
-            }
-            annotation.confirmDialogue.show();
-            var promises = ajax.call([
-                       {methodname: 'marktext', args:{component:'mod_moewiki', stringid: 'pluginname'}}
-                       ]);
-            promises[0].done(function(response) {
-                console.log('mod_wiki/pluginname is' + response);
-            }).fail(function(ex) {
-                // do something with the exception
-            });
-        },
-		merkannotaion : function(strings) {
-			var selection;
-			annotation.confirmDialogue = new M.core.dialogue({
-                headerContent: 'Annotation Form',
-                bodyContent: Y.one('#annotationFrom'),
-                draggable: true,
-                visible: false,
-                center: true,
-                modal: true,
-                width: null,
-                extraClasses: ['mod_mowiki_annotate_popup']
-            });
-			Y.one('#testmodal').on('click', annotation.displayDialogue);
-			$('.moewiki_content').mousedown(function() {
-				$('.moewiki_content').mouseup(function() {
-					try {
-			            if (selection = window.getSelection()) {
-			            	if(selection.type != 'Range'){
-			            		annotation.hide_annotaion(Y.one('#testmodal'));
-			            	} else {
-			            		annotation.show_annotaion(Y.one('#testmodal'),selection);
-			            	}
-			            }
-			        } catch (e) {
-			            /* give up */
-			        }
-				});
+		merkannotaion : function(params) {
+			var app = new annotator.App();
+			app.include(annotator.ui.main, {
+			    element: document.querySelector('.moewiki_content')
 			});
-
+			app.include(this.moodlestorage);
+			app.start().then(function () {
+			     app.annotations.load();
+			});
+			/*var promises = ajax.call([
+			    { methodname: 'moe_wiki_search', args: { wikiid: 186} }
+			]);
+			promises[0].done(function(response) {
+			       console.log('mod_moewiki/pluginname is' + response);
+			   }).fail(function(ex) {
+			       // do something with the exception
+				   console.log('mod_moewiki/pluginname is' + ex);
+			   });*/
 		},
-		hide_annotaion: function(element){
-			element.setStyles({display: 'none'});
-		},
-		show_annotaion: function(element, selection){
-			element.setStyles({
-				display: 'block',
-				top: selection.baseNode.parentElement.offsetTop});
-		},
-		displayDialogue: function(e) {
-            if (e) {
-                e.halt();
-            }
-            annotation.confirmDialogue.show();
-        },
+		moodlestorage: function () {
+			return {
+				create : function(annotaion) {
+					return this.ajaxcall('create', annotaion);
+				},
+				query : function(){
+					return this.search();
+				},
+				search: function(){
+					return this.ajaxcall('search', {});
+				},
+				ajaxcall: function(action,obj){
+					var id = obj && obj.id;
+					var url = this.options.prefix;
+					var data = {};
+					data.methodname = 'moe_wiki_'+action;
+					data.args = obj;
+					return ajax.call([data]);
+				},
+			};
+		}
 	};
 	return annotation;
 });
