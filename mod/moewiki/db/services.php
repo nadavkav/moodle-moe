1bd125d28e93eeeef5ac8c6a6990cff01acceb39
Avi Levy <avi@sysbind.co.il>
2016-05-05 09:47:46 +0300

    ticket 7 mod_moewiki: add update annotation

--- mod/moewiki/db/services.php
+++ mod/moewiki/db/services.php
@@ -25,11 +25,23 @@ $functions = array(
         'type'        => 'write',
         'ajax'        => 'true',
     ),
+    'moe_wiki_update' => array(
+        'classname'   => 'mod_moewiki_external',
+        'methodname'  => 'update',
+        'classpath'   => 'mod/moewiki/externallib.php',
+        'description' => 'Update annotation',
+        'type'        => 'write',
+        'ajax'        => 'true',
+    ),
 );
 
 $services = array(
     'MOE_wiki annotaions' => array(
-        'functions' => array('moe_wiki_search','moe_wiki_create','moe_wiki_delete'),
+        'functions' => array(
+            'moe_wiki_search',
+            'moe_wiki_create',
+            'moe_wiki_delete',
+            'moe_wiki_update',),
         'restrictedusers' => 0,
         'enabled'=>1,
         'requiredcapability' => '',
