caf94c72419cbd82c1685d4362ac72cd9d71f226
Avi Levy <avi@sysbind.co.il>
2016-07-11 17:07:45 +0300

    ticket 69 format_moetopcoll: insert labels in all sections

--- course/format/moetopcoll/format.php
+++ course/format/moetopcoll/format.php
@@ -68,7 +68,7 @@ if (($marker >= 0) && has_capability('moodle/course:setcurrentsection', $context
 // Make sure all sections are created.
 $courseformat = course_get_format($course);
 $course = $courseformat->get_course();
-course_create_sections_if_missing($course, range(0, $course->numsections));
+$courseformat->course_create_sections_if_missing($course, range(0, $course->numsections));
 
 $renderer = $PAGE->get_renderer('format_moetopcoll');
 
