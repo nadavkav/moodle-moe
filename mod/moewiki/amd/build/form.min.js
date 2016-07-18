70c845438da6a3efea1746b31d4be7336419f51b
Avi Levy <avi@sysbind.co.il>
2016-07-19 00:04:35 +0300

    ticket7 mod_moewiki: add build files

---
+++ mod/moewiki/amd/build/form.min.js
@@ -0,0 +1,27 @@
+/**
+ * @package    mod_moewiki
+ */
+
+/**
+ * @module mod_moewiki/form
+ */
+
+define(['jquery'], function($){
+	function Form() {
+		this.template_text = null;
+	}
+	
+	Form.prototype.init = function(){
+		var form = this;
+		$('#id_subwikis').change(function(){
+			if($(this).val() === 0){
+				form.template_text = $('#fitem_id_template_text').detach();
+			} else {
+				$('#fitem_id_template_file').after(form.template_text);
+				form.template_text = null;
+			}
+		});
+	};
+	
+	return new Form();
+});
