61a1060d3bbb1f7c9637532463815a13168ecf35
meir <meir@meir>
2016-11-27 10:19:54 +0200

    setting up as new block plugin

---
+++ blocks/rolespecifichtmlmoe/lang/fr/block_rolespecifichtml.php
@@ -0,0 +1,20 @@
+<?php
+
+$string['rolespecifichtmlmoe:addinstance'] = 'Peut ajouter une instance';
+
+$string['blockname'] = 'HTML (par rôle)';
+$string['pluginname'] = 'HTML (par rôle)';
+$string['newhtmlblock'] = 'Nouveau bloc Html par role';
+$string['leaveblanktohide'] = 'Laisser vide pour cacher';
+$string['configtitle'] = 'Titre visible du bloc';
+$string['configcontent'] = 'Contenu pour le rôle : {$a}';
+$string['configcontentforall'] = 'Contenu pour tous les rôles';
+$string['configcontext'] = 'Contexte';
+$string['configdisplay'] = 'Afficher';
+$string['showallmatches'] = 'tous les rôles endossés';
+$string['showhighest'] = 'le rôle le plus haut seulement';
+$string['system'] = 'Système';
+$string['course'] = 'Cours (courant)';
+$string['context'] = 'Contexte des rôles';
+$string['context_help'] = 'Si vous changez ce contexte, enregistrez d\'abord le changement puis revenez au formulaire pour éditer la liste de rôles mise à jour.';
+
