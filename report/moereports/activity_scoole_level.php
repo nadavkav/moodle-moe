f98b70036d014726b8bbd893b7a5a74390836b5d
meir <meir@sysbind.co.il>
2017-02-20 16:26:21 +0200

    ticket330 reports:moereport add excel import option

--- report/moereports/activity_scoole_level.php
+++ report/moereports/activity_scoole_level.php
@@ -15,22 +15,22 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 require_once('../../config.php');
 require_once('../../report/moereports/classes/local/peractivityschoollevel.php');
-
 require_once($CFG->libdir.'/completionlib.php');
 require_once($CFG->libdir.'/modinfolib.php');
-
+$download   = optional_param('download', '', PARAM_ALPHA);
 global $OUTPUT;
 
 $url = new moodle_url('/report/moereports/activity_scoole_level.php');
 $PAGE->set_url($url);
 
+if ($download !== '') {
+    $url->param('download', $download);
+}
 // Make sure that the user has permissions to manage moe.
 require_login();
 
 $context = context_system::instance();
 $PAGE->set_context($context);
-
-
 $PAGE->set_title(get_string('per_activity_school_level', 'report_moereports'));
 $PAGE->set_heading(get_string('per_activity_school_level', 'report_moereports'));
 $PAGE->set_pagelayout('standard');
@@ -38,14 +38,71 @@ $PAGE->set_pagelayout('standard');
 $results = new peractivityschoollevel();
 $data = new stdClass();
 $data->results = $results->displayreportfortemplates();
-
+$data->url="$url" . "?download=xls";
 $renderer = $PAGE->get_renderer('core');
-
 $resulttable = $OUTPUT->render_from_template('report_moereports/scool_level', $data);
+//print spreadsheet if one is asked for:
+if ($download == "xls" ) {
+    require_once("$CFG->libdir/excellib.class.php");
+    $date= date("Ymd");
+    /// Calculate file name
+    $filename = "$date"."_report";
+    /// Creating a workbook
+    $workbook = new MoodleExcelWorkbook("-");
+    /// Send HTTP headers
+    $workbook->send($filename);
+    /// Creating the first worksheet
+    // assigning by reference gives this: Strict standards: Only variables should be assigned by reference in /data_1/www/html/moodle/moodle/mod/choicegroup/report.php on line 157
+    // removed the ampersand.
+    $myxls = $workbook->add_worksheet("one");
+    /// Print names of all the fields
+    $myxls->write_string(0,0,get_string("region", 'report_moereports'));
+    $myxls->write_string(0,1,get_string("symbol", 'report_moereports'));
+    $myxls->write_string(0,2,get_string("name", 'report_moereports'));
+    $myxls->write_string(0,3,get_string("city", 'report_moereports'));
+    $myxls->write_string(0,4,get_string("cors", 'report_moereports'));
+    $myxls->write_string(0,5,get_string("activity", 'report_moereports'));
+    $myxls->write_string(0,6,get_string("makbila9", 'report_moereports'));
+    $myxls->write_string(0,7,get_string("percents9", 'report_moereports'));
+    $myxls->write_string(0,8,get_string("makbila10", 'report_moereports'));
+    $myxls->write_string(0,9,get_string("percents10", 'report_moereports'));
+    $myxls->write_string(0,10,get_string("makbila11", 'report_moereports'));
+    $myxls->write_string(0,11,get_string("percents11", 'report_moereports'));
+    $myxls->write_string(0,12,get_string("makbila12", 'report_moereports'));
+    $myxls->write_string(0,13,get_string("percents12", 'report_moereports'));
 
+
+    /// generate the data for the body of the spreadsheet
+    $i=0;
+    $row=1;
+    foreach ($data->results as $onerec){
+        $myxls->write_string($row, 0, $onerec->region);
+        $myxls->write_string($row, 1, $onerec->scollSymbol);
+        $myxls->write_string($row, 2, $onerec->scollName);
+        $myxls->write_string($row, 3, $onerec->city);
+        $myxls->write_string($row, 4, $onerec->course);
+        $myxls->write_string($row, 5, $onerec->activityname);
+        $myxls->write_string($row, 6, $onerec->ninthgradesum);
+        $myxls->write_string($row, 7, $onerec->ninthgradetotal);
+        $myxls->write_string($row, 8, $onerec->tenthgradesum);
+        $myxls->write_string($row, 9, $onerec->tenthgradetotal);
+        $myxls->write_string($row, 10, $onerec->eleventhgradesum);
+        $myxls->write_string($row, 11, $onerec->eleventhgradetotal);
+        $myxls->write_string($row, 12, $onerec->twelfthgradesum);
+        $myxls->write_string($row, 13, $onerec->twelfthgradetotal);
+        $row++;
+
+    }
+     
+    /// Close the workbook
+    $workbook->close();
+    exit;
+}
 echo $OUTPUT->header();
 echo $OUTPUT->heading(get_string('per_activity_school_level', 'report_moereports'));
 $PAGE->requires->js_call_amd('report_moereports/persistent_headers','init');
+$PAGE->requires->js_call_amd('report_moereports/ecxelexport','init');
+
 
 echo $resulttable;
 echo $OUTPUT->footer();
