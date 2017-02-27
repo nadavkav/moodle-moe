62d3811607915246a2415e044cfa588123af970d
meir <meir@sysbind.co.il>
2017-02-27 17:09:25 +0200

    ticket334 mod:quizsbs moving js to file and css chenges.

--- mod/quizsbs/attempt.php
+++ mod/quizsbs/attempt.php
@@ -126,7 +126,6 @@ if (!$attemptobj->set_currentpage($page)) {
 // Initialise the JavaScript.
 $headtags = $attemptobj->get_html_head_contributions($page);
 $PAGE->requires->js_init_call('M.mod_quizsbs.init_attempt_form', null, false, quizsbs_get_js_module());
-$PAGE->requires->js_call_amd('mod_quizsbs/headnavbuttons', 'init');
 $title = get_string('attempt', 'quizsbs', $attemptobj->get_attempt_number());
 $headtags = $attemptobj->get_html_head_contributions($page);
 $PAGE->set_title($attemptobj->get_quizsbs_name());
