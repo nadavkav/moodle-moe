61edc3ad8d26c9c15372849c9ad4f450223665c4
meir <meir@sysbind.co.il>
2017-05-22 16:06:49 +0300

    ticket366 report:moereport corse region level upgrade to new format

--- report/moereports/course_regin_level.php
+++ report/moereports/course_regin_level.php
@@ -14,84 +14,74 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 require_once('../../config.php');
-require_once('../../report/moereports/classes/local/percoursereginlevel.php');
-require_once($CFG->libdir.'/completionlib.php');
-require_once($CFG->libdir.'/modinfolib.php');
-$download   = optional_param('download', '', PARAM_ALPHA);
+require_once($CFG->dirroot . '/lib/dataformatlib.php');
 global $DB, $PAGE, $OUTPUT;
+
+$dataformat = optional_param('dataformat', null, PARAM_ALPHA);
 
 $url = new moodle_url('/report/moereports/course_regin_level.php');
 $PAGE->set_url($url);
 
-if ($download !== '') {
-    $url->param('download', $download);
-}
-
 require_login();
-
 $context = context_system::instance();
 $PAGE->set_context($context);
-
+$usercontext = context_user::instance($USER->id);
 
 $PAGE->set_title(get_string('per_course_regin_level', 'report_moereports'));
 $PAGE->set_heading(get_string('per_course_regin_level', 'report_moereports'));
 $PAGE->set_pagelayout('standard');
 
-$results = new percoursereginlevel();
 $data = new stdClass();
-$data->results = $results->displayreportfortemplates();
-$data->url="$url" . "?download=xls";
+$data->results = array();
+
+//set the user permition:
+$regions = array();
+$courses = $DB->get_records('course', array('enablecompletion' => '1'));
+if(is_siteadmin()|| has_capability('report/moereport:viewall', $usercontext)){
+    $data->results = $DB->get_records_sql('select * from mdl_moereports_courseregin');
+    $data->results = array_values($data->results);
+    foreach ($data->results as $rec){
+        unset($rec->id);
+    }
+} else {
+    $cond='where region in (';
+    $useryeshuyot = explode(',', $USER->profile['Yeshuyot']);
+    foreach ($useryeshuyot as $yeshut) {
+        $region = $DB->get_field('moereports_reports', 'region', array("symbol" => $yeshut));
+        if (!array_search($region, $regions)) {
+            array_push($regions, $region);
+            $cond = "$cond" . "$region . ,";
+        }
+
+    }
+    $cond = "$cond" . ")";
+    $data->results = $DB->get_records_sql("select * from mdl_moereports_courseregin" . "$cond");
+    $data->results = array_values($data->results);
+    foreach ($data->results as $rec){
+        unset($rec->id);
+    }
+}
+
 $renderer = $PAGE->get_renderer('core');
 $resulttable = $OUTPUT->render_from_template('report_moereports/course_regin_level', $data);
 
-//print spreadsheet if one is asked for:
-if ($download == "xls" ) {
-    require_once("$CFG->libdir/excellib.class.php");
-    $date= date("Ymd");
-    /// Calculate file name
-    $filename = "$date"."_report";
-    /// Creating a workbook
-    $workbook = new MoodleExcelWorkbook("-");
-    /// Send HTTP headers
-    $workbook->send($filename);
-    /// Creating the first worksheet
-    // assigning by reference gives this: Strict standards: Only variables should be assigned by reference in /data_1/www/html/moodle/moodle/mod/choicegroup/report.php on line 157
-    // removed the ampersand.
-    $myxls = $workbook->add_worksheet("one");
-    /// Print names of all the fields
-    $myxls->write_string(0,0,get_string("region", 'report_moereports'));
-    $myxls->write_string(0,1,get_string("cors", 'report_moereports'));
-    $myxls->write_string(0,2,get_string("makbila8", 'report_moereports'));
-    $myxls->write_string(0,3,get_string("percents8", 'report_moereports'));
-    $myxls->write_string(0,4,get_string("makbila9", 'report_moereports'));
-    $myxls->write_string(0,5,get_string("percents9", 'report_moereports'));
-    $myxls->write_string(0,6,get_string("makbila10", 'report_moereports'));
-    $myxls->write_string(0,7,get_string("percents10", 'report_moereports'));
-    
-    
-    /// generate the data for the body of the spreadsheet
-    $i=0;
-    $row=1;
-    foreach ($data->results as $onerec){
-            $myxls->write_string($row, 0, $onerec->region);
-            $myxls->write_string($row, 1, $onerec->course);
-            $myxls->write_string($row, 2, $onerec->eighthgradesum);
-            $myxls->write_string($row, 3, $onerec->eighthgradetotal);
-            $myxls->write_string($row, 4, $onerec->ninthgradesum);
-            $myxls->write_string($row, 5, $onerec->ninthgradetotal);
-            $myxls->write_string($row, 6, $onerec->tenthgradesum);
-            $myxls->write_string($row, 7, $onerec->tenthgradetotal);
-            $row++;
-            
-    }
-     
-    /// Close the workbook
-    $workbook->close();
-    exit;
-    }
+if ($dataformat != null){
+    $columns = array(
+        'region' => get_string('region','report_moereports'),
+        'cors' => get_string('cors','report_moereports'),
+        'makbila8' => get_string('makbila8','report_moereports'),
+        'percents8' => get_string('percents8','report_moereports'),
+        'makbila9' => get_string('makbila9','report_moereports'),
+        'percents9' => get_string('percents9','report_moereports'),
+        'makbila10' => get_string('makbila10','report_moereports'),
+        'percents10' => get_string('percents10','report_moereports'),
+    );
+    download_as_dataformat('activity_in_region' . date('c') , $dataformat, $columns, $data->results);
+}
 
 echo $OUTPUT->header();
 echo $OUTPUT->heading(get_string('per_course_regin_level', 'report_moereports'));
+echo $OUTPUT->download_dataformat_selector(get_string('excelexp', 'report_moereports'), '/report/moereports/course_regin_level.php', 'dataformat', array());
 
 echo "$resulttable";
 $PAGE->requires->js_call_amd('report_moereports/persistent_headers','init');
