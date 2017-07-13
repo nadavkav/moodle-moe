a8aa3a793b315b36786788d3fd6eea562d1249ac
meir ifrach <meir@sysbind.co.il>
2017-07-13 09:38:18 +0300

    ticket391 tt

--- local/notes/classes/local/notes_form.php
+++ local/notes/classes/local/notes_form.php
@@ -14,7 +14,7 @@ class notes_form extends \moodleform {
             $note = $this->_form; // Don't forget the underscore!
             $note->addElement('editor', 'content', get_string('draftbutton', 'moeworksheets'));
             $note->setType('content', PARAM_RAW);
-            //$note->setDefault('content', $this->content);
+            $note->setDefault('content', 'text from the note form class');
             $this->add_action_buttons(false,false);
      }
 }
