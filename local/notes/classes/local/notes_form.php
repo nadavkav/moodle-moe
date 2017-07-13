213811fb5786b39c5fcd224a23375c8e9ab5cfd5
meir ifrach <meir@sysbind.co.il>
2017-07-13 12:47:31 +0300

    ticket391 finish draft mechanism

--- local/notes/classes/local/notes_form.php
+++ local/notes/classes/local/notes_form.php
@@ -4,17 +4,12 @@ require_once("$CFG->libdir/formslib.php");
 
 class notes_form extends \moodleform {
 
-//     protected $content;
-//     public function __construct( $content) {
-//         $this->content = $content;
-//     }
-
         function definition() {
-            global $CFG, $content;
+            global $CFG;
             $note = $this->_form; // Don't forget the underscore!
             $note->addElement('editor', 'content', get_string('draftbutton', 'moeworksheets'));
             $note->setType('content', PARAM_RAW);
-            $note->setDefault('content', 'text from the note form class');
+            $note->setDefault('content', array('text' => $this->_customdata['content']));
             $this->add_action_buttons(false,false);
      }
 }
