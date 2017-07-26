89021cd57d06629abab310cb4a06e236b8a90c33
meir ifrach <meir@sysbind.co.il>
2017-07-26 13:21:38 +0300

    ticket391 add form options

--- local/notes/classes/local/notes_form.php
+++ local/notes/classes/local/notes_form.php
@@ -6,8 +6,17 @@ class notes_form extends \moodleform {
 
         function definition() {
             global $CFG;
+            $editoropstion = array(
+                'subdirs'=>0,
+                'maxbytes'=>0,
+                'maxfiles'=>0,
+                'changeformat'=>0,
+                'context'=>null,
+                'noclean'=>0,
+                'trusttext'=>0,
+                'enable_filemanagement' => true);
             $note = $this->_form; // Don't forget the underscore!
-            $note->addElement('static', 'heading','<h1>'.get_string('draftbutton', 'moeworksheets').'</h1>');
+            $note->addElement('static', 'heading','<h1>'.get_string('draftbutton', 'moeworksheets').'</h1>','',$editoropstion);
             $note->addElement('editor', 'content');
             $note->setType('content', PARAM_RAW);
             $note->setDefault('content', array('text' => $this->_customdata['content'], 'format' => FORMAT_HTML));
