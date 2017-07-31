b934c36979aab23d18de467eb777425b33c84a52
haim lavi <haim@sysbind.co.il>
2017-07-31 15:17:16 +0300

    moe_ticket193 theme/classes add style file

---
+++ theme/moe/classes/mod_quiz_renderer.php
@@ -0,0 +1,100 @@
+<?php
+
+//namespace theme_moe\output;
+
+//defined('MOODLE_INTERNAL') || die;
+
+//require_once($CFG->dirroot . '/mod/quiz/renderer.php');
+
+//use \mod_quiz\output;
+
+class theme_moe_mod_quiz_renderer extends mod_quiz_renderer
+{
+    /**
+     * Display a quiz navigation button.
+     *
+     * @param quiz_nav_question_button $button
+     * @return string HTML fragment.
+     */
+    protected function render_quiz_nav_question_button(quiz_nav_question_button $button) {
+        if (is_numeric($button->number)) {
+            $qnostring = 'questionnonav';
+        } else {
+            $qnostring = 'questionnonavinfo';
+        }
+
+        $classes = array('qnbutton', $button->stateclass, $button->navmethod, $qnostring);
+        $extrainfo = array();
+
+        if ($button->currentpage) {
+            $classes[] = 'thispage';
+            $extrainfo[] = get_string('onthispage', 'quiz');
+        }
+
+        // Flagged?
+        if ($button->flagged) {
+            $classes[] = 'flagged';
+            $flaglabel = get_string('flagged', 'question');
+        } else {
+            $flaglabel = '';
+        }
+        $extrainfo[] = \html_writer::tag('span', $flaglabel, array('class' => 'flagstate'));
+
+        $a = new \stdClass();
+        $a->number = $button->number;
+        $a->attributes = implode(' ', $extrainfo);
+        $tagcontents = \html_writer::tag('span', '', array('class' => 'thispageholder')) .
+            \html_writer::tag('span', '', array('class' => 'trafficlight')) .
+            get_string($qnostring, 'quiz', $a);
+        $tagattributes = array('class' => implode(' ', $classes), 'id' => $button->id,
+            'title' => $button->statestring, 'data-quiz-page' => $button->page);
+
+        if ($button->url) {
+            return \html_writer::link($button->url, $tagcontents, $tagattributes);
+        } else {
+            return \html_writer::tag('span', $tagcontents, $tagattributes);
+        }
+    }
+
+    /**
+     * Outputs the navigation block panel
+     *
+     * @param quiz_nav_panel_base $panel instance of quiz_nav_panel_base
+     */
+    public function navigation_panel(quiz_nav_panel_base $panel) {
+
+        $output = '';
+        $userpicture = $panel->user_picture();
+        if ($userpicture) {
+            $fullname = fullname($userpicture->user);
+            if ($userpicture->size === true) {
+                $fullname = html_writer::div($fullname);
+            }
+            $output .= html_writer::tag('div', $this->render($userpicture) . $fullname,
+                array('id' => 'user-picture', 'class' => 'clearfix'));
+        }
+        $output .= $panel->render_before_button_bits($this);
+
+        $bcc = $panel->get_button_container_class();
+        $output .= html_writer::start_tag('div', array('class' => "qn_buttons clearfix $bcc"));
+        $buttons = $panel->get_question_buttons($panel);
+        foreach ($buttons as $key => $button) {
+            if ($button instanceof \quiz_nav_section_heading) {
+                $output .= '<a href="'.$buttons[$key+1]->url->out().'">'.$this->render($button).'</a>';
+            } else {
+                $output .= $this->render($button);
+            }
+
+        }
+        $output .= html_writer::end_tag('div');
+
+        $output .= html_writer::tag('div', $panel->render_end_bits($this),
+            array('class' => 'othernav'));
+
+        $this->page->requires->js_init_call('M.mod_quiz.nav.init', null, false,
+            quiz_get_js_module());
+
+        return $output;
+    }
+
+}
