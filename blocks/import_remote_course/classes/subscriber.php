27b9e8c1e17ccb944079a0206db121ad500c516f
meir ifrach <meir@sysbind.co.il>
2017-08-01 14:48:34 +0300

    ticket395 add semi colom

--- blocks/import_remote_course/classes/subscriber.php
+++ blocks/import_remote_course/classes/subscriber.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-namespace blocks_import_remote_course
+namespace blocks_import_remote_course;
 class subscriber {
 
     /**
