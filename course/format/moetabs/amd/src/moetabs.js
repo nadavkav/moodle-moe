7bbaba6e7ff64c804944e441aebc42e3df1745d8
haim <haim@sysbind.co.il>
2017-08-10 11:20:18 +0300

    moe_ticket16 course/format/moetabs/amd/src background for section 0 list

--- course/format/moetabs/amd/src/moetabs.js
+++ course/format/moetabs/amd/src/moetabs.js
@@ -1,5 +1,5 @@
 /**
- * @package    format_moetabs
+ * @package format_moetabs
  * 
  */
 
@@ -11,18 +11,27 @@
 define(['jquery'], function() {
 
     var moetabs = function() {
+    	
+    	this.ruler = $('.format-moetabs ul.nav.nav-tabs');
 
-    	moetabs.prototype.init = function() {
+    	 moetabs.prototype.init = function() {
+    		
+    		if(this.ruler.width() >= $('.format-moetabs .course-content .single-section').width()){
+    			var error;
+    			error = 'need arrow';
+    		}
 
     		$( '.sectionzerobtn' ).click(function() {
     			$( '#gridshadebox_content' ).removeClass( 'hide_content');
     			$( '#gridshadebox_close' ).css("display", "inline");
     			$( '#gridshadebox_content' ).addClass( 'absolute' );
+    			$( '#gridshadebox_overlay' ).css("display", "block"); 
     		});
     		
     		$( '#gridshadebox_close' ).click(function() {
     			$( '#gridshadebox_content' ).addClass( 'hide_content' );
     			$( '#gridshadebox_content' ).removeClass( 'absolute');
+    			$( '#gridshadebox_overlay' ).css("display", "none");
     		});
     		
         };
