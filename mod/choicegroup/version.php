b48060e80414df0edbdcc6f46e7bc4e689462684
Avi Levy <avi@sysbind.co.il>
2017-08-18 18:50:28 +0300

    ticket400 mod_coicegroup: update module

--- mod/choicegroup/version.php
+++ mod/choicegroup/version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015121000;
+$plugin->version  = 2017050101;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.9 for Moodle 2.7-3.0 (Build: 2015121000)';
+$plugin->release = '1.9.6 for Moodle 2.7-3.3 (Build: 2017050101)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
