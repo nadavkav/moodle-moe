051dc728bed48c3cf38028a5feff4b064c0aad0c
haim <haim@sysbind.co.il>
2017-08-21 14:09:02 +0300

    moe_ticket16 course/format/moetabs fix responsive btn

--- course/format/moetabs/amd/src/moetabs.js
+++ course/format/moetabs/amd/src/moetabs.js
@@ -8,35 +8,59 @@
   */
 
 
-define(['jquery'], function() {
-
-    var moetabs = function() {
+define(['jquery'], function($) {
+		
+    var Moetabs = function() {
     	
-    	this.ruler = $('.format-moetabs ul.nav.nav-tabs');
+        this.ruler = $('.format-moetabs ul.nav.nav-tabs');
+       		
+    	Moetabs.prototype.init = function() {
+    		
 
-    	 moetabs.prototype.init = function() {
+    		$( window ).resize(function() {
+    			
+                var btnsize = $('.format-moetabs .sectionzerotext').width() - 
+    			( $('.format-moetabs .sectionzeroimg').width() +  
+    					 ( parseInt($(".format-moetabs .sectionzeroimg").css("margin-left").replace("px","")) * 2 ) );
+                var minbtnsize = 610;
+ 			    if ( btnsize < minbtnsize ) {
+    				$( '.format-moetabs .sectionzerobtn' ).css("position", "unset");
+    				$('.format-moetabs .littlesquare').hide();
+    				var containersize = $('.format-moetabs .sectionzerotext').width();
+    				$( '.format-moetabs .sectionzerobtn' ).css("width", containersize); 
+    			} else {
+    				$( '.format-moetabs .sectionzerobtn' ).css("position", "absolute"); 
+    				$('.format-moetabs .littlesquare').show();
+    				if ( window.fullScreen ) {
+          			  $( '.format-moetabs .sectionzerobtn' ).css("width", "766px");
+      				} else {
+      					$( '.format-moetabs .sectionzerobtn' ).css("width", btnsize );
+      				}
+    				
+    			} 
+    		});
     		
     		if(this.ruler.width() >= $('.format-moetabs .course-content .single-section').width()){
     			var error;
     			error = 'need arrow';
     		}
-
-    		$( '.sectionzerobtn' ).click(function() {
-    			$( '#gridshadebox_content' ).removeClass( 'hide_content');
-    			$( '#gridshadebox_close' ).css("display", "inline");
-    			$( '#gridshadebox_content' ).addClass( 'absolute' );
-    			$( '#gridshadebox_overlay' ).css("display", "block"); 
+    		   		
+    		$( '.format-moetabs .sectionzerobtn' ).click(function() {
+    			$( '.format-moetabs #gridshadebox_content' ).removeClass( 'hide_content');
+    			$( '.format-moetabs #gridshadebox_close' ).css("display", "inline");
+    			$( '.format-moetabs #gridshadebox_content' ).addClass( 'absolute' );
+    			$( '.format-moetabs #gridshadebox_overlay' ).css("display", "block"); 
     		});
     		
-    		$( '#gridshadebox_close' ).click(function() {
-    			$( '#gridshadebox_content' ).addClass( 'hide_content' );
-    			$( '#gridshadebox_content' ).removeClass( 'absolute');
-    			$( '#gridshadebox_overlay' ).css("display", "none");
+    		$( '.format-moetabs #gridshadebox_close' ).click(function() {
+    			$( '.format-moetabs #gridshadebox_content' ).addClass( 'hide_content' );
+    			$( '.format-moetabs #gridshadebox_content' ).removeClass( 'absolute');
+    			$( '.format-moetabs #gridshadebox_overlay' ).css("display", "none");
     		});
     		
         };
 
     };
 
-    return new moetabs();
+    return new Moetabs();
 });
