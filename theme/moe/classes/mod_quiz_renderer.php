52d71196cd60c09b654f83ae2956700f16d2064e
meir ifrach <meir@sysbind.co.il>
2017-10-24 10:39:45 +0300

    ticket435 theme:moe change end of test notification to bold

--- theme/moe/classes/mod_quiz_renderer.php
+++ theme/moe/classes/mod_quiz_renderer.php
@@ -19,6 +19,62 @@ defined('MOODLE_INTERNAL') || die;
 
 class theme_moe_mod_quiz_renderer extends mod_quiz_renderer
 {
+	
+	/**
+	 * Creates any controls a the page should have.
+	 *
+	 * @param quiz_attempt $attemptobj
+	 */
+	public function summary_page_controls($attemptobj) {
+		$output = '';
+		
+		// Return to place button.
+		if ($attemptobj->get_state() == quiz_attempt::IN_PROGRESS) {
+			$button = new single_button(
+					new moodle_url($attemptobj->attempt_url(null, $attemptobj->get_currentpage())),
+					get_string('returnattempt', 'quiz'));
+			$output .= $this->container($this->container($this->render($button),
+					'controls'), 'submitbtns mdl-align');
+		}
+		
+		// Finish attempt button.
+		$options = array(
+				'attempt' => $attemptobj->get_attemptid(),
+				'finishattempt' => 1,
+				'timeup' => 0,
+				'slots' => '',
+				'sesskey' => sesskey(),
+		);
+		
+		$button = new single_button(
+				new moodle_url($attemptobj->processattempt_url(), $options),
+				get_string('submitallandfinish', 'quiz'));
+		$button->id = 'responseform';
+		if ($attemptobj->get_state() == quiz_attempt::IN_PROGRESS) {
+			$button->add_action(new confirm_action(get_string('confirmclose', 'quiz'), null,
+					get_string('submitallandfinish', 'quiz')));
+		}
+		
+		$duedate = $attemptobj->get_due_date();
+		$message = '';
+		if ($attemptobj->get_state() == quiz_attempt::OVERDUE) {
+			$message ="<p class='olympicmark'>" . get_string('overduemustbesubmittedby', 'quiz', userdate($duedate)) . "</p>";
+			
+		} else if ($duedate) {
+			$message = get_string('mustbesubmittedby', 'quiz', userdate($duedate));
+		}
+		
+		$output .= $this->countdown_timer($attemptobj, time());
+		$output .= $this->container($message . $this->container(
+				$this->render($button), 'controls'), 'submitbtns mdl-align');
+		
+		return $output;
+	}
+	
+	
+	
+	
+	
     /**
      * Display a quiz navigation button.
      *
