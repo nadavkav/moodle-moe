73cd805d0a22fbd8009fb69be143e5cb9d405676
meir ifrach <meir@sysbind.co.il>
2017-12-11 16:46:10 +0200

    fix subscriber cast to int

--- blocks/import_remote_course/classes/subscriber.php
+++ blocks/import_remote_course/classes/subscriber.php
@@ -183,7 +183,7 @@ class subscriber {
             	$dataobject->section	     = $section;
             	
                 foreach ($coursewithtamplayte as $course) {
-                	if ($exist = notification_helper::get_record(['courseid' => (int)$course->course_id, 'cm' => (int)$cm]) > 0) {
+                	if ($exist = notification_helper::get_record(['courseid' => (int)$course->course_id, 'cm' => (int)$cm])) {
                 		$notification = new notification_helper($exist->id);
                 		$notification->delete();
                 	}
