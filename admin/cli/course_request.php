22e6d9d7e1e55081b497759630fb4189636912e6
meir ifrach <meir@sysbind.co.il>
2017-12-27 15:06:17 +0200

    ticket455 change to context system and add printing

--- admin/cli/course_request.php
+++ admin/cli/course_request.php
@@ -32,16 +32,38 @@
 define('CLI_SCRIPT', true);
 require(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once($CFG->libdir.'/clilib.php');
+cli_writeln('starting rule assign script...');
 
 $users = $DB->get_records_sql("select * from {user} where id in (select uid.userid from {user_info_data} uid INNER JOIN {user_info_field} uif 
     ON uid.fieldid = uif.id where uif.shortname ='IsStudent' and uid.data = 'NO' and uid.userid in (select userid from {user_info_data} uid INNER JOIN 
     {user_info_field} uif ON uid.fieldid = uif.id where uif.shortname = 'SimpleRole' and uid.data like '%667%'))");
+cli_writeln("found " . count($users) . " users to update");
 
 $roleid = $DB->get_field('role', 'id', ['shortname'=>'course_request']);
+cli_writeln('the rule ID is: ' . $roleid);
+
+$contextid = $DB->get_field('context', 'id', ['contextlevel' => CONTEXT_SYSTEM, 'depth' => 1]);
+cli_writeln('the context ID is: ' . $contextid);
+
+$totalsuccess = 0;
+$totalskipd = 0;
 foreach ($users as $user){
-    $addrole = ['roleid'=>$roleid, 'contextid'=>'1','userid'=>$user->id ,'timemodified'=>time(), 'modifierid'=>'2'];
-    $exist = $DB->get_records('role_assignments', ['userid'=>$user->id, 'roleid'=>$roleid]);
+    $addrole = ['roleid' => $roleid, 'contextid' => $contextid,'userid' => $user->id ,'timemodified' => time(), 'modifierid' => '2'];
+    $exist = $DB->get_records('role_assignments', ['userid' => $user->id, 'roleid' => $roleid]);
     if (empty($exist)){
+        cli_writeln("adding user with ID  $user->id");
         $DB->insert_record('role_assignments', $addrole);
+        $totalsuccess++;
+    } else {
+        cli_writeln("user with ID $user->id already enroll. skips!");
+        $totalskipd++;
     }
 }
+
+cli_writeln('script finish successfully');
+cli_writeln('-------------------------------------');
+cli_writeln('total user processed:' . count($users));
+cli_writeln('total users enrolled:' . $totalsuccess);
+cli_writeln('total users skipped:' . $totalskipd);
+
+
